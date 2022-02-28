803ae4cc4ed21f0f25d21f3d1a52d2acd88b9c96
MifuneKinski <appacademystudent@Apps-Mac-mini-2.local>
2022-02-28 13:54:22 -0500

    did hash set

--- hash_and_lru/lib/p03_hash_set.rb
+++ hash_and_lru/lib/p03_hash_set.rb
@@ -7,18 +7,33 @@ class HashSet
   end
 
   def insert(key)
+    resize! if count >= num_buckets
+    hash = key.hash
+    unless include?(key)
+      self[hash] << key
+      @count += 1
+    end
   end
 
   def include?(key)
+    hash = key.hash
+    return false unless self[hash].include?(key)
+    true
   end
 
   def remove(key)
+    hash = key.hash
+    if include?(key)
+      self[hash].delete(key) 
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +41,13 @@ class HashSet
   end
 
   def resize!
+    new_num_buckets = num_buckets * 2
+    new_store = Array.new(new_num_buckets) {Array.new}
+    @store.each do |bucket|
+      bucket.each do |num|
+        new_store[num.hash % new_num_buckets] << num
+      end
+    end
+    @store = new_store
   end
 end
