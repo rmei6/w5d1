49b654b71fcd83a335df36771649b82441bea26a
MifuneKinski <appacademystudent@Apps-Mac-mini-2.local>
2022-02-28 13:43:30 -0500

    did hashing

--- hash_and_lru/lib/p02_hashing.rb
+++ hash_and_lru/lib/p02_hashing.rb
@@ -4,11 +4,15 @@ end
 
 class Array
   def hash
+    self.inject(0.hash) {|acc,num| acc = acc.hash ^ num.hash}
   end
 end
 
 class String
   def hash
+    arr = self.split("").to_a
+    arr.map! {|char| char.ord}
+    arr.hash
   end
 end
 
@@ -16,6 +20,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    #0
+    sorted = self.sort.to_h
+    key_hash = sorted.keys.map{|ele| ele.to_s.ord}.hash
+    value_hash = sorted.values.map{|ele| ele.to_s.ord}.hash
+    key_hash ^ value_hash   #examples make key_hash and value_hash equal
   end
 end
